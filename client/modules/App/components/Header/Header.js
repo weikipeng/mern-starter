50e6b578d8f25283c4beae6577452e48c27b9cb7
wikipeng <pengjunwei@100tal.com>
2017-06-25 19:51:29 +0800

    update head

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,24 +1,18 @@
-import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
-import { FormattedMessage } from 'react-intl';
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+import {FormattedMessage} from 'react-intl';
+import classNames from 'classnames/bind';
 
 // Import Style
 import styles from './Header.css';
 
+let cx = classNames.bind(styles);
+
+let headerClass = cx("page-header", "navbar", "navbar-fixed-top");
+
 export function Header(props, context) {
   return (
-    <div className={styles.header}>
-      <div className={styles.content}>
-        <h1 className={styles['site-title']}>
-          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
-        </h1>
-        {
-          context.router.isActive('/', true)
-            ? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>
-            : null
-        }
-      </div>
-    </div>
+      <div className = {headerClass}></div>
   );
 }
 
@@ -33,3 +27,16 @@ Header.propTypes = {
 };
 
 export default Header;
+
+// <div className={styles.header}>
+//   <div className={styles.content}>
+//     <h1 className={styles['site-title']}>
+//       <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
+//     </h1>
+//     {
+//       context.router.isActive('/', true)
+//         ? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>
+//         : null
+//     }
+//   </div>
+// </div>
